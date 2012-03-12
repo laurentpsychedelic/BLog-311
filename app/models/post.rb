24bde8d6c6551f426d524ce814410d10b416bf7f
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-03-12 14:49:05 +0900

    Add validations rules for new posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+  validates :body,  :presence => true,
+                    :length => { :maximum => 140 } #Twitter-like body length limitation
 end
