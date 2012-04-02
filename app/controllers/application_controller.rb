2ded5d1fa0f4e083eeb8376095b101cc2bb90bc1
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-04-02 10:19:23 +0900

    Add an embryonic authentication step to display/authorize creating/editing or deleting posts
    The real authentication process must be also implemented from now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :admin?
+  protected
+  def authorize
+  end
+  
+  def admin?
+    return false
+  end
 end
