db0427bad86a1510e760a8683d49d23615f67467
Laurent FABRE <laurent_psychedelic@hotmail.com>
2012-04-16 21:42:44 +0900

    1-Remove the maximum characters limit for a post (Twitter like)
    It was just for experimenting purposes.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
   validates :body,  :presence => true,
-                    :length => { :maximum => 140 } #Twitter-like body length limitation
+                    :length => { :minimum => 10 }
 end
